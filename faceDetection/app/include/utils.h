fc0c24043f5465a38adb4af537018306f592af96
Nick Ma <nick.mayerhofer@hotmail.com>
2011-05-06 11:34:42 +0200

    ready for first upload with counters

--- faceDetection/app/include/utils.h
+++ faceDetection/app/include/utils.h
@@ -1,7 +1,7 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
-#define COUNTER_COUNT 7
-
+#define COUNTER_COUNT 5
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
 #endif
--- faceDetection/app/src/skinFilter.c
+++ faceDetection/app/src/skinFilter.c
@@ -12,18 +12,13 @@
 #define CB_HIGH 0.05f
 #define CR_HIGH 0.20f
 
-extern module_handle_t counterHandle[COUNTER_COUNT];
-
 void skinFilter(image_t *inputImage, image_t *outputImage)
 {
 	int x, y;
 	for (y = 0; y < inputImage->height; ++y) {
 		for (x = 0; x < inputImage->width; ++x) {
 			
-			counter_start(&counterHandle[6]);
-			counter_start(&counterHandle[7]);
 			ycbcr_color_t ycbcr = getYCbCrColorValue(inputImage, x, y);
-			counter_stop(&counterHandle[7]);
 			int pIndex = (y*inputImage->width+x)*3;
 			
 			if (ycbcr.y >= Y_LOW && ycbcr.y <= Y_HIGH
@@ -40,7 +35,6 @@ void skinFilter(image_t *inputImage, image_t *outputImage)
 				outputImage->data[pIndex+1] = 0x00;
 				outputImage->data[pIndex+2] = 0x00;
 			}
-			counter_stop(&counterHandle[6]);
 		}
 	}
 }
