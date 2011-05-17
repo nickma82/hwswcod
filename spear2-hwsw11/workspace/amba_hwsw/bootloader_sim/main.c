d307a26ba1434c71e91cc7f3b52b0c9e09f06efb
Johannes Kasberger <schugabe@gmx.at>
2011-05-17 12:48:06 +0200

    farbkonvertierung in hw fertig

--- spear2-hwsw11/workspace/amba_hwsw/bootloader_sim/main.c
+++ spear2-hwsw11/workspace/amba_hwsw/bootloader_sim/main.c
@@ -12,35 +12,31 @@
 
 #define ALUEXT_BASE 		(0xFFFFFE80)
 
-#define ALUEXT_OP_A	(*(volatile int *const)(ALUEXT_BASE+4))
-#define ALUEXT_OP_B	(*(volatile int *const)(ALUEXT_BASE+8))
-#define ALUEXT_RES	(*(volatile int *const)(ALUEXT_BASE+12))
-#define ALUEXT_CMD	(*(volatile int *const)(ALUEXT_BASE+16))
+#define ALUEXT_R	(*(volatile uint8_t *const)(ALUEXT_BASE+4))
+#define ALUEXT_G	(*(volatile uint8_t *const)(ALUEXT_BASE+5))
+#define ALUEXT_B	(*(volatile uint8_t *const)(ALUEXT_BASE+6))
+#define ALUEXT_Y	(*(volatile int32_t *const)(ALUEXT_BASE+8))
+#define ALUEXT_CB	(*(volatile int32_t *const)(ALUEXT_BASE+12))
+#define ALUEXT_CR	(*(volatile int32_t *const)(ALUEXT_BASE+16))
 
-#define ALUEXT_MULT 0x01
 
-/*
-int mult(int a,int b) {
-	volatile int *reg = (int *)(ALUEXT_BASE +12);
-	volatile uint32_t *reg1 = (uint32_t *)(ALUEXT_BASE+4);
-	volatile uint32_t *reg2 = (uint32_t *)(ALUEXT_BASE+8);
-	*reg1 = a;
-	*reg2 = b;
-	//ALUEXT_CMD  = ALUEXT_MULT;
-	asm("nop");
-	return *reg;
-}*/
-//#define COUNTER_BADDR ((uint32_t)-320)
 
 int main (int argc, char *argv[])
 {
-  //int tmp = mult(10,10);
-  //volatile uint32_t *reg = (uint32_t *)(COUNTER_BADDR+8);
-  //return *reg;
-  volatile uint32_t *reg1 = (uint32_t *)(ALUEXT_BASE+4);
-  volatile uint32_t *reg2 = (uint32_t *)(ALUEXT_BASE+12);
-  *reg1 = 10;
-  int asdf = *reg2;
-  return asdf;
+	ALUEXT_R = 100;
+	ALUEXT_G = 255;
+	ALUEXT_B = 10;
+	/*int32_t rf = (mult(1000,cl.r)) >> 8;
+	int32_t gf = (mult(1000,cl.g)) >> 8;
+	int32_t bf = (mult(1000,cl.b)) >> 8;
+	
+	result.y = mult(299000,rf) + mult(587000,gf) + mult(114000,bf);
+	result.cb = mult(-168736,rf) + mult(-331264,gf) + mult(500000,bf);
+	result.cr = mult(500000, rf) + -mult(418688,gf) + mult(-81312, bf);*/
+	int32_t tmp;
+	tmp = ALUEXT_Y;
+	tmp = ALUEXT_CB;
+	tmp = ALUEXT_CR;
+	return 0;
 }
 
