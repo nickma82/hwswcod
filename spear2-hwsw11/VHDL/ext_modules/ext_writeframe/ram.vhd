e22197edd2b87765b0fae6bcd563469d2fe87453
Nick Ma <nick.mayerhofer@hotmail.com>
2011-06-08 14:23:07 +0200

    fifo nonsense changes

--- spear2-hwsw11/VHDL/ext_modules/ext_writeframe/ram.vhd
+++ spear2-hwsw11/VHDL/ext_modules/ext_writeframe/ram.vhd
@@ -1,8 +1,14 @@
+-----------------------------------------------------------------------------
+-- Author:      Nick Mayerhofer
+-- Description: Asynchronous FIFO with generic width
+-- Date:		08.06.2011
+-----------------------------------------------------------------------------
+
 library ieee; 
 use ieee.std_logic_1164.all; 
 use ieee.numeric_std.all;
 
-
+---- RAM
 entity dp_ram is
 	generic
 	(
@@ -44,3 +50,46 @@ begin
 	end process;
 	
 end architecture beh;
+
+
+--library ieee; 
+--use ieee.std_logic_1164.all; 
+--use ieee.numeric_std.all;
+--
+--USE ieee.math_real.log2;
+--USE ieee.math_real.ceil;
+--
+------ FIFO
+--entity async_fifo is
+--	generic
+--	(
+--		BUFFER_LENGTH : integer range 1 to integer'high;
+--		DATA_WIDTH : integer range 1 to integer'high
+--	);
+--	port
+--	(
+--		--global
+--		reset		: in std_logic;
+--		fifo_empty	: out std_logic;
+--		fifo_full	: out std_logic;
+--		--in
+--		i_data 		: in std_logic_vector(DATA_WIDTH - 1 downto 0);
+--		i_en		: in  std_logic;
+--		i_val       : out std_logic;
+--		--out
+--		o_data	 	: out std_logic_vector(DATA_WIDTH - 1 downto 0);
+--		o_en		: in  std_logic;
+--		o_val       : out std_logic
+--		
+--	);
+--end entity async_fifo;
+--
+--
+--architecture afifo of async_fifo is
+--	--#calc ADDD = (BUFFER_LENGTH//2)/(2//2)
+--	constant  ADDRESS_WIDTH : integer := INTEGER(CEIL( LOG2(REAL(BUFFER_LENGTH-1))/LOG2(REAL(2)) ));
+--	
+--	signal tmp_reset : std_logic;
+--begin
+--	tmp_reset <= reset;
+--end architecture afifo;
